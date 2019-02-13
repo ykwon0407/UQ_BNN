6db64857b6a6516e85f9ea761ecd6a4a49424042
Yongchan Kwon <ykwon0407@snu.ac.kr>
2019-02-13 17:05:57 +0900

    appendix

--- retina/model.py
+++ retina/model.py
@@ -34,7 +34,8 @@ def precision_smooth(y_true, y_pred):
     return (intersection / (K.sum(y_pred_f) + K.epsilon()))  
 
 def UNet(N_filters=32):
-    '''U net architecture (down/up sampling with skip architecture)
+    '''
+    U net architecture (down/up sampling with skip architecture)
     '''
     from keras.layers import Input, Dropout, Activation, Add, BatchNormalization, Conv2D, Concatenate, UpSampling2D
     def Conv2DReluBatchNorm(n_filters, kernel_size, strides, inputs):
@@ -72,3 +73,46 @@ def UNet(N_filters=32):
 
     from keras.models import Model
     return Model(inputs=inputs, outputs=output)
+
+
+
+def Small_UNet(N_filters=32):
+    '''
+    U net architecture (down/up sampling with skip architecture)
+    '''
+    from keras.layers import Input, Dropout, Activation, Add, BatchNormalization, Conv2D, Concatenate, UpSampling2D
+    def Conv2DReluBatchNorm(n_filters, kernel_size, strides, inputs):
+        x = Conv2D(n_filters, (1,1), strides=1, padding='same', 
+                   kernel_initializer='he_normal',
+                   activation='elu')(inputs)
+        x = Conv2D(n_filters, kernel_size, strides=strides, padding='same', 
+                   kernel_initializer='he_normal',
+                   activation='elu')(x)
+        x = BatchNormalization()(x)
+        x = Dropout(rate=0.25)(x, training=True) # training + test-time dropout!
+        return x
+
+    inputs = Input((None, None, 3))
+    
+    layer1 = Conv2DReluBatchNorm(N_filters,  (5, 5), (1,1), inputs)
+    layer2 = Conv2DReluBatchNorm(2*N_filters,  (5, 5), (2,2), layer1)
+    layer3 = Conv2DReluBatchNorm(4*N_filters, (3, 3), (2,2), layer2)
+    layer4 = Conv2DReluBatchNorm(8*N_filters, (3, 3), (2,2), layer3)
+    
+    merge5 = Concatenate(axis=-1)([UpSampling2D(size=(2,2))(layer4), layer3])
+    layer5 = Conv2DReluBatchNorm(4*N_filters, (3, 3), (1,1), merge5)
+
+    merge6 = Concatenate(axis=-1)([UpSampling2D(size=(2,2))(layer5), layer2])
+    layer6 = Conv2DReluBatchNorm(2*N_filters, (3, 3), (1,1), merge6)
+
+    merge7 = Concatenate(axis=-1)([UpSampling2D(size=(2,2))(layer6), layer1])
+    layer7 = Conv2DReluBatchNorm(N_filters, (3, 3), (1,1), merge7)
+
+    output = Conv2D(1, (1, 1), strides=(1,1), activation='sigmoid', name='output')(layer7)    
+
+    from keras.models import Model
+    return Model(inputs=inputs, outputs=output)  
+
+
+
+
